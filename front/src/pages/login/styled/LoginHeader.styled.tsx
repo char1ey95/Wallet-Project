787345e8ec29e321c44904c96370ce46bbd233f7
char1ey <ckstn410@gmail.com>
2023-05-18 10:19:11 +0900

    fixed: login 리팩터링

--- front/src/pages/login/styled/LoginHeader.styled.tsx
+++ front/src/pages/login/styled/LoginHeader.styled.tsx
@@ -1,7 +1,6 @@
-import { Icon } from '@iconify/react'
 import styled from 'styled-components'
 
-const LoginHeaderWrapper = styled.header`
+export const LoginHeaderWrapper = styled.header`
     padding: 1rem;
     min-height: 10rem;
     width: 100%;
@@ -9,7 +8,7 @@ const LoginHeaderWrapper = styled.header`
     box-sizing: border-box;
 `
 
-const LoginHeaderIcon = styled.div`
+export const LoginHeaderIcon = styled.div`
     width: 100%;
     height: 3.5rem;
     & > svg {
@@ -20,25 +19,13 @@ const LoginHeaderIcon = styled.div`
     }
 `
 
-const LoginHeaderMainContent = styled.h2`
+export const LoginHeaderMainContent = styled.h2`
     margin-top: 1rem;
     font-family: 'Gmarket';
 `
 
-const LoginHeaderSubContent = styled.h3`
+export const LoginHeaderSubContent = styled.h3`
     margin-top: 0.5rem;
     font-family: 'Gmarket';
     color: #9c9c9c;
 `
-
-export const LoginHeader = () => {
-    return(
-        <LoginHeaderWrapper>
-            <LoginHeaderIcon>
-                <Icon icon={'material-symbols:account-balance-wallet'}></Icon>
-            </LoginHeaderIcon>
-            <LoginHeaderMainContent>Char1ies Wallet</LoginHeaderMainContent>
-            <LoginHeaderSubContent>Extension</LoginHeaderSubContent>
-        </LoginHeaderWrapper>
-    )
-}
